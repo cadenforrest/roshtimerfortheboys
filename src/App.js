62d081a7055e29af429d5653d0d6abc04914d01d
cadenforrest <cadenforrest@gmail.com>
2021-04-19 17:28:08 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const React = require('react');
+const prettyMilliseconds = require('pretty-ms'); 
+class App extends React.Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      time: 0,
+      start: 0,
+      isOn: false
+    }
+    this.startTimer = this.startTimer.bind(this)
+    this.stopTimer = this.stopTimer.bind(this)
+    this.resetTimer = this.resetTimer.bind(this)
+  }
+  startTimer() {
+    this.setState({
+      time: this.state.time,
+      start: Date.now() - this.state.time,
+      isOn: true
+    })
+    this.timer = setInterval(() => this.setState({
+      time: Date.now() - this.state.start
+    }), 1);
+  }
+  stopTimer() {
+    this.setState({isOn: false})
+    clearInterval(this.timer)
+  }
+  resetTimer() {
+    this.setState({time: 0})
+  }
+  render() {
+    let start = (this.state.time == 0) ?
+      <button onClick={this.startTimer}>start</button> :
+      null
+    let stop = (this.state.isOn) ?
+      <button onClick={this.stopTimer}>stop</button> :
+      null
+    let reset = (this.state.time != 0 && !this.state.isOn) ?
+      <button onClick={this.resetTimer}>reset</button> :
+      null
+    let resume = (this.state.time != 0 && !this.state.isOn) ?
+      <button onClick={this.startTimer}>resume</button> :
+      null
+    return(
+      <div>
+        <h3>timer: {prettyMilliseconds(this.state.time, {colonNotation:true})}</h3>
+        {start}
+        {resume}
+        {stop}
+        {reset}
+      </div>
+    )
+  }
 }
-
-export default App;
+export default App
